3cb4cde72d735d7b54ca6b699212a9dd75e9baaf
Thellenxa <117475016+Thellenxa@users.noreply.github.com>
2025-03-11 16:38:31 +0100

    ///

--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -15,29 +15,35 @@ namespace SportsStore.Controllers {
             repository = repo;
         }
 
-        public ViewResult List(string category, int productPage = 1)
-            => View(new ProductsListViewModel {
+        public ActionResult List(string category, int productPage = 1)
+        {
+            var productsListViewModel = new ProductsListViewModel
+            {
                 Products = repository.Products
                     .Where(p => category == null || p.Category == category)
                     .OrderBy(p => p.ProductID)
                     .Skip((productPage - 1) * PageSize)
                     .Take(PageSize),
-                PagingInfo = new PagingInfo {
+                PagingInfo = new PagingInfo
+                {
                     CurrentPage = productPage,
                     ItemsPerPage = PageSize,
                     TotalItems = category == null ?
                         repository.Products.Count() :
-                        repository.Products.Where(e =>
-                            e.Category == category).Count()
+                        repository.Products.Where(e => e.Category == category).Count()
                 },
                 CurrentCategory = category
-            });
-        
-       
+            };
+
+            return View(productsListViewModel); 
+        }
+
+
+
         public ActionResult Index()
         {
-
-            return View(_context.Products.ToList());
+            var products = _context.Products.ToList();  
+            return View(products);  
         }
         [HttpGet]
         public ActionResult Create()
